8fdfc4aa725d346823ce23bd7b77658930592b0e
tkealamakia <tkealamakia@gmail.com>
2016-09-05 17:45:29 -0600

    Add collider script

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCollider : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D trigger) {
+		print ("Trigger");
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		print ("Collision");
+	}
+		
+}
